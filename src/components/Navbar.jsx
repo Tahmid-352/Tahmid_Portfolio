eac5b53a6607bbc5d303f7b734e7a829d7bd86b1
Tahmid-352 <tahmidkhan017433@gmail.com>
2024-08-01 05:11:23 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../assets/Tahmid_logo.png'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img className='mx-2 w-16' src={logo} alt="logo" style={{height: "80px"}} />
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitterSquare />
+        <FaInstagram />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
